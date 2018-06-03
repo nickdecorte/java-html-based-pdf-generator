993755b548323c73619cf3c31a4e026ca26e8c9a
Nick Decorte <nickdecorte@live.be>
2018-06-03 10:30:09 +0200

    Made a fully working document

--- src/main/java/com/example/documents/http/GenerateController.java
+++ src/main/java/com/example/documents/http/GenerateController.java
@@ -2,14 +2,13 @@ package com.example.documents.http;
 
 import com.example.documents.domain.Document;
 import com.example.documents.generator.DocumentGenerator;
+import com.example.documents.generator.DocumentTemplateRenderer;
 import com.itextpdf.text.DocumentException;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,26 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Arrays;
 
 @RestController
 public class GenerateController {
+
+    @Autowired
+    private DocumentTemplateRenderer documentTemplateRenderer;
 
     @Autowired
     private DocumentGenerator documentGenerator;
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public ResponseEntity<byte[]> generate(@RequestBody Document document) throws IOException, DocumentException {
-//        String request = "{ \"data\":{ \"company\": \"acme\", \"date\": \"04/05/2018\", \"products\": [{ \"name\": \"Coca Cola\", \"price\": \"1.80\" },{ \"name\": \"Sprite\", \"price\": \"1.90\" }] } }";
-//
-//        Document document = new Document();
-//        document.setFileName("test.pdf");
-//        document.setTemplate("invoice/master");
-//        document.setLanguage("nl");
-//        document.setData(new ObjectMapper().readValue(request, HashMap.class));
-//        document.addTemplateParameter("be");
-//        document.addTemplateParameter("acme");
-
         ByteArrayOutputStream outputStream = documentGenerator.create(document);
 
         HttpHeaders headers = new HttpHeaders();
@@ -45,6 +37,16 @@ public class GenerateController {
         headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
 
         return new ResponseEntity<byte[]>(outputStream.toByteArray(), headers, HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String generateTest() throws IOException, DocumentException {
+        Document document = new Document();
+        document.setType("declarationoforigin");
+        document.setSpecifiers(Arrays.asList("be"));
+
+        return documentTemplateRenderer.render(document);
     }
 
 }
